9d8981382fdb0ac00e9de3f567a85064e555e583
PraveerAchra44 <achra.praveer@gmail.com>
2023-08-07 21:49:22 +0530

    adjusted the script according to new changes

--- Projects/Stone_paper_scissors_GAME/script.js
+++ Projects/Stone_paper_scissors_GAME/script.js
@@ -1,14 +1,7 @@
 //get DOM elements
 
 const gameContainer = document.querySelector(".gradient-container"),
-    userResult = document.querySelector(".user_image img"),
-    compResult = document.querySelector(".comp_image img"),
-    result = document.querySelector(".result"),
-    optionImages = document.querySelectorAll(".option_image");
-
-optionImages.forEach((image,index)=>{
-    image.addEventListener("click",(e)=>{
-        image.classList.add("active");
-    })
-})
-
+userResult = document.querySelector(".user_result img"),
+compResult = document.querySelector(".comp_result img"),
+result = document.querySelector(".result"),
+optionImages = document.querySelectorAll(".option_image")
