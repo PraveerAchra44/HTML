20bb82aa4952dc12db323eac6703228c8dde866c
PraveerAchra44 <achra.praveer@gmail.com>
2023-08-19 10:14:36 +0530

    addition of JavaScript

--- Projects/digital clock/script.js
+++ Projects/digital clock/script.js
@@ -1,35 +1,18 @@
+// let hours = document.querySelector(".hours");
+// hours.innerHTML.fo
+// let mins = document.querySelector(".mins");
+// let seconds = document.querySelector(".seconds");
 
-setInterval(()=>{
-    let d = new Date()
-    let h = d.getUTCHours()
-    let m = d.getUTCMinutes()
-    let s = d.getUTCSeconds()
-    let week_day = d.getDay()
-    if(week_day == "1"){
-        day.innerHTML = "Sunday"
-    }
-    if(week_day == "2"){
-        day.innerHTML = "Monday"
-    }
-    if(week_day == "3"){
-        day.innerHTML = "Tuesday"
-    }
-    if(week_day == "4"){
-        day.innerHTML = "Wednesday"
-    }
-    if(week_day == "5"){
-        day.innerHTML = "Thursday"
-    }
-    if(week_day == "6"){
-        day.innerHTML = "Friday"
-    }
-    if(week_day == "7"){
-        day.innerHTML = "Satday"
-    }
 
-    hours.innerHTML = h
-    mins.innerHTML = m
-    seconds.innerHTML = s
-    
+let hrs = document.getElementById("hours");
+let mins = document.getElementById("mins");
+let sec = document.getElementById("seconds");
 
-},1000)
+setInterval(() => {
+    let currentTime = new Date()
+
+    hrs.innerHTML = (currentTime.getHours()<10?"0":"")+currentTime.getHours();
+    mins.innerHTML = (currentTime.getMinutes()<10?"0":"")+currentTime.getMinutes();
+    sec.innerHTML = (currentTime.getSeconds()<10?"0":"")+currentTime.getSeconds();
+
+}, 1000)
