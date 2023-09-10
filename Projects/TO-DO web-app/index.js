e7511ac5fe8b05dd77355ea3e337078edd209bb7
PraveerAchra44 <achra.praveer@gmail.com>
2023-09-10 09:48:03 +0530

    logic implemented

--- Projects/TO-DO web-app/index.js
+++ Projects/TO-DO web-app/index.js
@@ -1,12 +1,29 @@
 import express from "express"
+import bodyParser from "body-parser";
+import ejs from "ejs";
+
 
 const app = express();
 const port = 3000;
 
-app.get("/",(req,res)=>{
-    res.render("index.ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let newItems = [];
+app.get("/", (req, res) => {
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var date = new Date();
+    let day = date.toLocaleDateString("en-US", options);
+    // console.log(today.toLocaleDateString("en-US", options));
+
+    res.render("index.ejs",{today:day, newListItems:newItems});
 })
 
-app.listen(port,()=>{
+app.post("/",(req,res)=>{
+    let newitem = req.body.newitem;
+    newItems.push(newitem);
+    res.redirect("/");
+})
+
+app.listen(port, () => {
     console.log(`listening on ${port}.`);
 })
